10bc657052f0e8607d81756e60183cc2ba5777d4
rondoor124 <rondoor124@gmail.com>
2022-11-28 11:44:45 -0600

    update movement tests

--- tests/all-scene.test.js
+++ tests/all-scene.test.js
@@ -70,25 +70,54 @@ describe.only('should load scene works', () => {
           }
         });
 
-        const appCount = await getAppCountFromScene(sceneUrl)
+        displayLog('step', 'Should scene load works: ', 'trying to avatar moving');
+
+        await page.waitForTimeout(2000);
+
+        const firstPosition = await page.evaluate(async () => {
+          return globalWebaverse.playersManager.localPlayer.position;
+        });
+
+        const keys = ['KeyW', 'KeyA', 'KeyS', 'KeyD'];
+        const key = keys[Math.floor(Math.random() * keys.length)];
+        await page.keyboard.down(key);
+        await page.waitForTimeout(1000);
+        const playerMove = await page.evaluate(
+          async ({firstPosition, key}) => {
+            const avatar = globalWebaverse.playersManager.localPlayer.avatar;
+            const currentSpeed = avatar.velocity.length();
+            const idleWalkFactor = avatar.idleWalkFactor;
+            const currentPosition = globalWebaverse.playersManager.localPlayer.position;
+            return {
+              currentSpeed,
+              idleWalkFactor,
+              currentPosition,
+              // isCorrectMove,
+            };
+          },
+          {firstPosition, key},
+        );
+        await page.keyboard.up(key);
+        await page.waitForTimeout(3000);
 
         displayLog('step', 'Should scene load works: ', 'Validation checking');
-
         const errorLists = getErrorList()
-
         if (result.isSceneLoaded && errorLists.length === 0) {
           displayLog('success', 'Screen loaded properly');
         } else {
           displayLog('error', 'Screen not properly loaded, Please check out the browser console');
         }
 
+        const appCount = await getAppCountFromScene(sceneUrl)
         if (result.loadedAppCount !== appCount) {
           displayLog('error', 'Child apps not fully loaded: ', `${result.loadedAppCount} of ${appCount}`);
         } else {
           displayLog('success', 'Child apps fully loaded: ', `${result.loadedAppCount} of ${appCount}`);
         }
 
-        isSuccess = result.isSceneLoaded && result.loadedAppCount === appCount && errorLists.length === 0;
+        displayLog(playerMove.currentPosition !== firstPosition? 'success' : 'error', 'Should scene load works: ', 'Player avatar moved');
+
+        isSuccess = result.isSceneLoaded && result.loadedAppCount === appCount && errorLists.length === 0 && playerMove.currentPosition !== firstPosition;
 
         if (isSuccess) {
           displayLog('passed', 'Scene loaded successfully: ', `${sceneUrl}`);
